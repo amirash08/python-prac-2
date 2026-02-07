186a10eb6e59a39b73896fd40e48d67309800c8b
Amir Ashlyayev <a_ashlyaev@kbtu.kz>
2026-02-07 12:53:46 +0500

    Add first Python examples

---
+++ SYNTAX.py
@@ -0,0 +1,13 @@
+def ex1():
+        if 5 > 2: 
+          print ("Five is greater than two!")
+def ex2():
+      # This is a comment
+      x = 5
+      y = "Hello, World!"
+      print (x, y)
+choice = int(input("1 or 2: "))
+if choice == 1:
+    ex1()
+else:
+    ex2()
---
+++ statements.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+print ("Hello, world!") ; print ("Have a good day.") ; print ("Learning Python is fun!")
