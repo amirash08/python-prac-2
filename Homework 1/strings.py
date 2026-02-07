9a792b0023ee163a7739ff2862190bba77499483
Amir Ashlyayev <a_ashlyaev@kbtu.kz>
2026-02-07 13:39:43 +0500

    Add first Python examples

---
+++ Homework 1/strings.py
@@ -0,0 +1,34 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#Or three single quotes; 
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+#Get the character at position 1 (remember that the first character has the position 0):
+a = "Hello, World!"
+print(a[1])
+#Loop through the letters in the word "banana":
+for x in "banana":
+  print(x)
+#The len() function returns the length of a string: 
+a = "Hello, World!"
+print(len(a))
+#Check if "free" is present in the following text:
+txt = "The best things in life are free!"
+print("free" in txt)
+#Print only if "free" is present:
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  #Print only if "expensive" is NOT present:
+  txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
