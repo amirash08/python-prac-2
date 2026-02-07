e50534fb431da4e8a742d26ff8e834f024665694
Amir Ashlyayev <a_ashlyaev@kbtu.kz>
2026-02-07 14:16:49 +0500

    Add first Python examples

--- Homework 2/memrship_operators.py
+++ Homework 2/memrship_operators.py
@@ -1,3 +1,13 @@
 #Check if "banana" is present in a list:
 fruits = ["apple", "banana", "cherry"]
 print("banana" in fruits)
+
+fruits = ["apple", "banana", "cherry"]
+
+print("pineapple" not in fruits)
+
+text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
