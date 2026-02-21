fbf00dd218077d1b9b9214ad7bdf547bf5ce8a50
Amir Ashlyayev <a_ashlyaev@kbtu.kz>
2026-02-21 11:40:56 +0500

    Add Homework 4

--- Homework 4/generators.py
+++ Homework 4/generators.py
@@ -27,3 +27,48 @@ print(next(iter)) #3
 print(next(iter)) #4
 print(next(iter)) #StopIteration error
 
+class MyNumbers:   
+    def iter(self):
+        self.a = 1
+        return self
+
+    def next(self):
+        if self.a > 5:            # остановка после 5
+            raise StopIteration
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+#Generators 
+def mygen(): #generator function
+    n = 1 #generator object is created but the function is not yet executed
+    print('This is printed first') #the function is executed until the first yield statement
+    yield n #the function is paused and the value of n is returned to the caller
+
+    n += 1 
+    print('This is printed second')
+    yield n
+
+    n += 1
+    print('This is printed at last')
+    yield n
+
+a = mygen() #generator object is created but the function is not yet executed
+print(next(a)) #This is printed first 1
+print(next(a)) #This is printed second 2
+print(next(a)) #This is printed at last 3
+
+my_generator = (num for num in range (1, 1000)) #generator expression
+print (sys.getsizeof(my_generator)) #size of generator object in bytes
+for num in my_generator: #iterating through the generator object
+    print(num) #1
+    
