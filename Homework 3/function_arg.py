449c4bd7efc038c83001b892559f773034793776
Amir Ashlyaev <a_ashlyaev@kbtu.kz>
2026-02-21 16:46:19 +0500

    Update files

---
+++ Homework 3/function_arg.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+print (add(2,3))
+
+def full_name(first, second):
+    return first + " " + second
+print (full_name("Amir", "Ashlyaev"))
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
