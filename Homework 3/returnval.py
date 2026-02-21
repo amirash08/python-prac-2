6efce4de64d70d01930ea549092c67c6373c75f8
Amir Ashlyaev <a_ashlyaev@kbtu.kz>
2026-02-21 17:02:40 +0500

    Update files

---
+++ Homework 3/returnval.py
@@ -0,0 +1,20 @@
+def dounle(x):
+    return x*2
+print (double(7))
+
+def shout(word):
+    return wurd.upper() + "!"
+print (shout("hello"))
+
+def check_age(age):
+    if age < 18:
+        return "Young"
+    return "adult"
+print (check_age(16))
+print (check_age(25))
+
+def min_max(a,b):
+    return min(a,b) , max(a,b)
+mn,mx = min_max(5,2)
+print(mn,mx)
+
